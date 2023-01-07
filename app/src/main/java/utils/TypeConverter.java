45dd508ea0efeb4d3cd844c1bd81c30054ef0264
Jim <jinchglin@gmail.com>
2023-01-07 14:57:25 +0800

    feat: type converter
    int array and big integer array converter

---
+++ app/src/main/java/utils/TypeConverter.java
@@ -0,0 +1,19 @@
+package utils;
+
+import java.math.BigInteger;
+import java.util.Arrays;
+
+public class TypeConverter {
+    public static BigInteger[] intsToBigIntegers(int[] ints) {
+        return Arrays.stream(ints)
+                .mapToObj(BigInteger::valueOf)
+                .toArray(BigInteger[]::new);
+    }
+
+    public static int[] bigIntegersToInts(BigInteger[] bigIntegers) {
+        return Arrays.stream(bigIntegers)
+                .mapToInt(BigInteger::intValue)
+                .toArray();
+    }
+
+}
